5a7e7cdab8e61312efc915b6f017c75274f2fa43
emmanuelreyes28 <63267318+emmanuelreyes28@users.noreply.github.com>
2021-03-10 15:39:42 -0800

    First Commit

---
+++ Assets/Main/Scripts/ScriptableObjectsCode/Items/Misc.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Default", menuName = "CustomObject/Item/Default")]
+public class Misc : Item
+{
+    readonly public ItemType itemType = ItemType.Misc;
+}
---
+++ Assets/Main/Scripts/UI/DialogUIManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogUIManager : MonoBehaviour
+{
+    public GameObject background;
+    public GameObject mainText;
+    public GameObject responseTab;
+    public GameObject continueText;
+    public List<Button> responsesHolder;
+
+    [SerializeField]
+    private int currentIndex = 0;
+    [SerializeField]
+    private int responses = 0;
+
+    private DialogueBranch branch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DeactiveDialogue();
+    }
+
+    public void ActiveDialogue()
+    {
+        background.SetActive(true);
+        mainText.SetActive(true);
+        continueText.SetActive(true);
+        responseTab.SetActive(true);
+        foreach (Button response in responsesHolder)
+        {
+            response.gameObject.SetActive(false);
+        }
+    }
+
+    public void DeactiveDialogue()
+    {
+        background.SetActive(false);
+        mainText.SetActive(false);
+        continueText.SetActive(false);
+        responseTab.SetActive(false);
+        foreach (Button response in responsesHolder)
+        {
+            response.gameObject.SetActive(false);
+        }
+    }
+
+    public void NextBranch(int branchSelect)
+    {
+        // Add ReciveDialogueBranch with newBranch being next branch
+        RecieveDialogueBranch(branch.ResponseOption[branchSelect].nextBranch);
+        ActiveDialogue();
+        NextDialogue();
+    }
+
+    
+    public void RecieveDialogueBranch(DialogueBranch newBranch)
+    {
+        // Add branch info here
+        this.branch = newBranch;
+        responses = Mathf.Clamp(branch.ResponseOption.Count,0,3);
+        currentIndex = 0;
+    }
+
+    public void NextDialogue()
+    {
+        //if at the end of the branch
+        if(currentIndex >= branch.DialogueLines.Count){
+            //No reponse -> End Dialogue
+            if(responses == 0){
+                DeactiveDialogue();
+            }
+        }
+        //DeactiveDialogue(); // Remove this and
+       // add next Dialogue mechanism here
+
+    }
+}
